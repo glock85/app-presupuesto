0734cc3141dd84d93e5faaf69202f6eecc98fe20
Joseph López <gerardomarcanoopsu2016@gmail.com>
2022-03-07 19:33:18 -0400

    app-presupuesto

---
+++ src/components/Pregunta.js
@@ -0,0 +1,77 @@
+import React, {Fragment, useState} from 'react';
+import Error from './Error';
+import PropTypes from 'prop-types';
+
+
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+
+    //definir el state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+  
+    //Funcion que lee el presupuesto
+  
+    const definirPresupuesto = e => {
+      guardarCantidad (parseInt(e.target.value, 10));
+    }
+
+    // Submit para definir el presupuesto
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        //Validar
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+
+
+        //Si se pasa la validacion
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+  
+  
+    return ( 
+
+        <Fragment>
+
+            <h2>Escribe tu miserable presupuesto</h2>
+
+            {error ? <Error mensaje="El presupuesto es incorrecto, no sabes ni hacer eso bien"/> : null}
+
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+
+                <input 
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto"
+                />
+
+            </form>
+
+        </Fragment>
+
+     );
+}
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+ 
+export default Pregunta;
